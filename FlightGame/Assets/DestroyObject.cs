567081791058ca360db5b5c0b10c64d40a914bd1
Filipe Moreira <filimor@posteo.com.br>
2020-02-08 02:25:42 -0300

    Particle system

---
+++ FlightGame/Assets/DestroyObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace RollingBall
+{
+    public class DestroyObject : MonoBehaviour
+    {
+        private void Start()
+        {
+            Invoke("DeleteObject", 1.5f);
+        }
+
+        private void DeleteObject()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ FlightGame/Assets/Felpudo/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace FlightGame
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private bool _started;
+        //private bool _finished;
+        private Rigidbody2D _playerBody;
+        private Vector2 _impulseForce = new Vector2(0, 500f);
+
+        public GameObject featherParticle;
+
+        private void Start()
+        {
+            _playerBody = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                if (!_started)
+                {
+                    _started = true;
+                    _playerBody.isKinematic = false;
+                }
+
+                _playerBody.velocity = new Vector2(0, 0);
+                _playerBody.AddForce(_impulseForce);
+
+                GameObject feathers = Instantiate(featherParticle);
+                Vector3 playerPosition = transform.position + new Vector3(0, 1, 0);
+                featherParticle.transform.position = transform.position;
+            }
+
+            float posFelpudoPx = Camera.main.WorldToScreenPoint(transform.position).y;
+
+            if (posFelpudoPx > Screen.height || posFelpudoPx < 0)
+            {
+                Destroy(gameObject);
+            }
+
+            transform.rotation = Quaternion.Euler(0, 0, _playerBody.velocity.y * 2.1f);
+        }
+    }
+}
